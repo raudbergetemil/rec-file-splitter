06053b6cbde96cc60de789bf9a2cce73f7fd766f
raudbergetemil <raemil@student.chalmers.se>
2019-11-08 16:15:49 +0100

    Restructured code

--- cpp/rec_splitter.cpp
+++ cpp/rec_splitter.cpp
@@ -11,35 +11,92 @@
 #include <string>
 #include <fstream>
 
-
-void writeMessageToFile(std::ofstream &fd, cluon::data::Envelope env){
-  std::string data = cluon::serializeEnvelope(std::move(env));
-
-  fd.write(&data[0], data.length());
-}
+// Function pointer declaration
+void writeMessageToFile(std::ofstream &fd, cluon::data::Envelope);
+int fileType(std::string);
+int cutDirectory(std::string, double, double);
+int cutFile(std::string, double, double);
 
 int main(int argc, char **argv) {
 
   double start_time_relative, time_interval;
-
-  /*if (argc < 2) {
+/*
+  if (argc < 2) {
     std::cerr << "Use: " << argv[0] << " FILENAME [START_TIME] [TIME_INTERVAL]" << std::endl;
     exit(-1);
   }
   else if(argc == 2){
+    // Default values if only filename is given
     time_interval = 5.0;
     start_time_relative = 0.0;
   }
   else {
     std::cerr << "Use: " << argv[0] << " FILENAME [START_TIME] [TIME_INTERVAL]" << std::endl;
     exit(-1);
-  }*/
+  }
+*/
 
   time_interval = 5.0;
   start_time_relative = 0.0;
 
-  std::string filename_out = "example_out.rec";
+  std::string filename_in = "test.rec";
+  int type = fileType(filename_in);
+
+  // If directory, cut all eligible files into the given interval
+  if (type == 0){
+    cutDirectory(filename_in, start_time_relative, time_interval);
+  }
+  // If filename_in is a file then cut it
+  else if (type == 1){
+    cutFile(filename_in, start_time_relative, time_interval);
+  }
+  else if (type == 2){
+    std::cout << "interpreted filename as invalid rec file" << std::endl;
+  }
+  return 0;
+}
+
+
+// Write envelope data to file
+void writeMessageToFile(std::ofstream &fd, cluon::data::Envelope env){
+  std::string data = cluon::serializeEnvelope(std::move(env));
+  fd.write(&data[0], data.length());
+}
+
+// Determines if given filename is a .rec-file or a directory
+// Also checks if it is the output from a previous split
+// Returns 0 if filename refers to a directory
+// 1 if it is a valid .rec-file
+// 2 if it is an invalid .rec-file
+// NOTE: This function is allergic to filenames that contains the string
+// '_rec'
+int fileType(std::string filename){
+  char *current_token = strtok(&filename[0], "_.");
+
+  if (current_token != NULL){
+    while(current_token != NULL){
+
+      std::cout << current_token << std::endl;
+      if (strcmp(current_token, "out") == 0){
+        return 2;
+      }
+      else if (strcmp(current_token, "rec") == 0){
+        return 1;
+      }
+      current_token = strtok(NULL, "_.");
+    }
+  }
+  return 0;
+}
+
+// Cut file 'filename_in' and put output in 'filename_in_out'
+int cutFile(std::string filename_in, double start_time_relative, double time_interval){
+  std::string tmpstr;
+  strcpy(&tmpstr[0], &filename_in[0]);
+
+  std::string filename_out = strcat(strtok(&tmpstr[0], "."), "_out.rec");
   std::ofstream f_out(filename_out, std::ios::out | std::ios::binary);
+
 
   if (!f_out){
     std::cout << "Cannot open output file! " << std::endl;
@@ -48,7 +105,6 @@ int main(int argc, char **argv) {
 
   bool firstEnvelope = true;
   double timestamp, start_time, stop_time;
-  std::string filename_in = "test.rec";
 
   // We will use cluon::Player to parse and process a .rec file.
   constexpr bool AUTO_REWIND{false};
@@ -75,32 +131,6 @@ int main(int argc, char **argv) {
 
         firstEnvelope = false;
       }
-/*
-      // Check whether it is of type GeodeticWgs84Reading.
-      if (env.dataType() == opendlv::proxy::GeodeticWgs84Reading::ID()) {
-        // Unpack the content...
-        auto msg = cluon::extractMessage<opendlv::proxy::GeodeticWgs84Reading>(std::move(env));
-
-      }
-
-      // Check whether it is of type TestMessage2.
-      if (env.dataType() == odcore::testdata::TestMessage2::ID()) {
-        // Unpack the content...
-        auto msg = cluon::extractMessage<odcore::testdata::TestMessage2>(std::move(env));
-
-        // ...and traverse its data fields using a lambda function as alternative.
-        msg.accept([](uint32_t, const std::string &, const std::string &) {},
-                   [](uint32_t, std::string &&, std::string &&n, auto v) { std::cout << n << " = " << +v << std::endl; },
-                   [](){});
-      }
-
-      // Check whether it is of type TestMessage5.
-      if (env.dataType() == odcore::testdata::TestMessage5::ID()) {
-        auto msg = cluon::extractMessage<odcore::testdata::TestMessage5>(std::move(env));
-        msg.accept([](uint32_t, const std::string &, const std::string &) {},
-                   [](uint32_t, std::string &&, std::string &&n, auto v) { std::cout << n << " = " << v << std::endl; },
-                   [](){});
-      } */
 
       if (timestamp >= start_time && timestamp <= stop_time){
         writeMessageToFile(f_out, env);
@@ -114,4 +144,8 @@ int main(int argc, char **argv) {
   }
 
   return 0;
+}
+
+int cutDirectory(std::string dir_name, double start_time_relative, double time_interval){
+  std::cout << "NOT IMPLEMENTED" << std::endl;
 }
